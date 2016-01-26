ec893080d78beb3b44f08b800db57f397b539926
zinglax <dylanzingler@gmail.com>
2016-01-26 00:15:55 -0500

    adding a file to help with apache config

---
+++ SiteGenerator/sitedeployment.py
@@ -0,0 +1,10 @@
+apache_conf_list = [subdomain_name,
+					document_root, # Directory where code is located (Dummy File)
+					static_file_dir, # Alias for /static
+					static_file_dir, # Directory /static
+					wsgi_dir, # Directory with wsgi.py inside
+					manage_py_dir, # Python Path 
+					virtual_env_site_packages, # Site Packages directory of virutal environment,
+					wsgi_file, # Actual wsgi file path. 
+
+]
